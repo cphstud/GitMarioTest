f7f799043fecefdfa4efebf3a3017a3dd02bf62c
thor <thor@cphbusiness.dk>
2020-10-28 21:44:12 +0100

    main run

--- src/mario/cph/Main.java
+++ src/mario/cph/Main.java
@@ -2,6 +2,8 @@ package mario.cph;
 
 public class Main {
     public static void main(String[] args) {
+        MainController mainController = new MainController();
+        mainController.startOrdering();
 
     }
 }
