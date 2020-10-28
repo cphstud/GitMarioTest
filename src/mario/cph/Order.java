7a74eb42d7e6f31ece276e6ff12af6b9b62d79b9
thor <thor@cphbusiness.dk>
2020-10-28 21:22:46 +0100

    order

--- src/mario/cph/Order.java
+++ src/mario/cph/Order.java
@@ -1,4 +1,19 @@
 package mario.cph;
 
+
+import java.util.ArrayList;
+import java.util.List;
+import java.time.LocalDateTime;
+
 public class Order {
+    private final List<Pizza> pizzas;
+    private final LocalDateTime ldt;
+
+    Order() {
+        ldt = LocalDateTime.now();
+        pizzas = new ArrayList<>();
+    }
+
+
+
 }
